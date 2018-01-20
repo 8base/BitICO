988ddc765131bf7f1bc98efa19e22f57c4444589
Daniel Prada <daniel@lamusica.com>
2018-01-20 16:23:29 -0500

    Minor style tweaks

--- src/routes/admin/Admin.js
+++ src/routes/admin/Admin.js
@@ -13,17 +13,50 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Admin.css';
 
 const fields = [
-  'Token Logo',
-  'Token Name',
-  'Token Ticker',
-  'Total Supply',
-  'Fund Supply',
-  'Soft Cap',
-  'Hard Cap',
-  'Fund Start Date',
-  'Fund End Date',
-  'RSK Address',
-  'BTC Value Per Token',
+  {
+    name: 'Token Logo',
+    type: 'file',
+  },
+  {
+    name: 'Token Name',
+    type: 'text',
+  },
+  {
+    name: 'Token Ticker',
+    type: 'text',
+  },
+  {
+    name: 'Total Supply',
+    type: 'number',
+  },
+  {
+    name: 'Allocation',
+    type: 'number',
+  },
+  {
+    name: 'Soft Cap',
+    type: 'number',
+  },
+  {
+    name: 'Hard Cap',
+    type: 'number',
+  },
+  {
+    name: 'Fund Start Date',
+    type: 'date',
+  },
+  {
+    name: 'Fund End Date',
+    type: 'date',
+  },
+  {
+    name: 'RSK Address',
+    type: 'textarea',
+  },
+  {
+    name: 'BTC Value Per Token',
+    type: 'number',
+  },
 ];
 
 class Admin extends React.Component {
@@ -40,7 +73,7 @@ class Admin extends React.Component {
     this.state = {};
 
     fields.forEach(f => {
-      this.state[f] = '';
+      this.state[f.name] = '';
     });
 
     this.handleChange = this.handleChange.bind(this);
@@ -56,19 +89,34 @@ class Admin extends React.Component {
   }
 
   render() {
+    const formLabelStyle = {
+      display: 'block',
+      fontSize: '1.2em',
+      fontWeight: 400,
+      fontFamily: 'Helvetica',
+    };
+
+    const labelStyle = {
+      display: 'block',
+      fontSize: '1.1em',
+      fontWeight: 400,
+      fontFamily: 'Helvetica',
+    };
+
     return (
       <div className={s.root}>
         <form onSubmit={() => Admin.handleSubmit()} className={s.container}>
           <h1>{this.props.title}</h1>
           {fields.map(f => (
-            <fieldset key={f}>
-              <label htmlFor={f}>
-                {f}:
+            <fieldset key={f.name} style={{ padding: '1em 0em' }}>
+              <label htmlFor={f.name}>
+                <span style={formLabelStyle}>{f.name}</span>
                 <input
-                  type="text"
-                  value={this.state[f]}
-                  id={f}
+                  type={f.type}
+                  value={this.state[f.name]}
+                  id={f.name}
                   onChange={e => this.handleChange(e)}
+                  style={labelStyle}
                 />
               </label>
             </fieldset>
